03f6da5b09170978217fa0462421616961269276
m.bach@hq-imaging.de <m.bach@hq-imaging.de>
2022-11-28 12:59:41 +0100

    initial commit

---
+++ js/checkday.js
@@ -0,0 +1,18 @@
+var currentDate = new Date();
+
+function checkday(id) {
+	
+  const cbox = document.getElementById(id);
+  const vis = document.getElementById(id+'a');
+  //alert(id.slice(1));
+  
+//    if (cbox.checked == true && parseInt(id.slice(1)) <= currentDate.getDate() ){
+    if (cbox.checked == true && parseInt(id.slice(1)) <= 0 ){
+		vis.style.opacity = 1;
+		vis.style.transform = 'scale(1.5,1.5)';
+	} else {
+		cbox.checked = false
+		vis.style.opacity = 0;
+	}
+  
+}
